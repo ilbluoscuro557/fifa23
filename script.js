2e9e293c1af06ad1c86751d67211bb393f8cc2e7
ilbluoscuro557 <gaetanixlaporta@gmail.com>
2026-03-22 01:38:10 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const bottonehome = document.getElementById("bottone");
+bottonehome.addEventListener("click", function() {
+    window.location.href = "home.html";
+});
+
+const bottone_back = document.getElementById("bottone_in");
+bottone_back.addEventListener("click", function(){
+    window.location.href = "player.html";
+})
